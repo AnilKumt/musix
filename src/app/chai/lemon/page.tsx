83c57bd101d78fa62f2268ddd0885dc1c2932956
AnilKumt <anilkumt51@gmail.com>
2025-08-27 09:41:57 +0530

    learning layouts in next js and dong navbar

---
+++ src/app/chai/lemon/page.tsx
@@ -0,0 +1,5 @@
+export default function LemonPage(){
+    return(
+        <h1 className="text-7xl text-center mt-3.5">Lemon Tea</h1>
+    )
+}
---
+++ src/app/chai/page.tsx
@@ -0,0 +1,5 @@
+export default function ChaiPage(){
+    return(
+        <h1 className="text-7xl text-center mt-3.5">Chai page</h1>
+    )
+}
