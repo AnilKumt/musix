32696deee3216c44bf113be0e825c88d54c066ef
AnilKumt <anilkumt51@gmail.com>
2025-08-27 17:08:24 +0530

    completed sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <h1 className="text-4xl text-center text-white">MUSIC WORLD</h1>
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   );
 }
